f6ba7511a789d34285f083e7f6e4f0fa75881866
Kiran Kandel <theknownofficial@gmail.com>
2021-05-21 09:31:55 +0545

    Added basic routes to project

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ app.get('/api/v1/bootcamps', (req, res) => {
     res.status(200).json({ success: true, msg: 'Show All Bootcamps' });
 })
 
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Show ${req.params.id} Bootcamp` });
+})
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Show All Bootcamps' });
+})
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Display Bootcamp ${req.params.id}` });
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Delete Bootcamp ${req.params.id}` });
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`);

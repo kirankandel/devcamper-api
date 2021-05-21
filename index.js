ff1fae5c3f7754e863bdafa3bb7f6f3bbf7b14f0
Kiran Kandel <theknownofficial@gmail.com>
2021-05-21 09:26:44 +0545

    added prettier and eslint

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 //Load environment variables
-dotenv.config({path:'./config/config.env'});
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
 app.use(express.json());
 
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Show All Bootcamps' });
+})
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 })

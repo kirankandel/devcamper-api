1c18d9ccfaf65036b84df881fc83f5df5ccad75c
Kiran Kandel <theknownofficial@gmail.com>
2021-05-21 09:14:46 +0545

    addition of dotenv configuration

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+//Load environment variables
+dotenv.config({path:'./config/config.env'});
 
 const app = express();
 
 app.use(express.json());
 
-app.listen('5000', ()=>{
-    console.log('Listening to port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+    console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 })

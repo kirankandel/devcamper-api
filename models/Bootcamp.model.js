8ff473130634b7cb76ad5d2f6a2882d9326c9c93
Kiran Kandel <theknownofficial@gmail.com>
2021-05-22 15:25:52 +0545

    add geolocation geocoding function

--- models/Bootcamp.model.js
+++ models/Bootcamp.model.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-useless-escape */
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+const geocoder = require('../utils/geocoder.utils');
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -97,6 +99,31 @@ const BootcampSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     },
+});
+
+//Create Bootcamp Slug from Name
+BootcampSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+});
+
+// Geocode and create location field
+BootcampSchema.pre('save', async function (next) {
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+        type: 'Point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress,
+        street: loc[0].streetName,
+        city: loc[0].city,
+        state: loc[0].stateCode,
+        zipcode: loc[0].zipcode,
+        country: loc[0].countryCode,
+    }
+    //Do not save address to DB 
+    this.address = undefined;
+    
+    next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
